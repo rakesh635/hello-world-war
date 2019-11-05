d0e4b41bd9d8d2ab854106dea3da53b1bf6b6144
rakesh635 <rakesh635@gmail.com>
2019-11-05 23:30:31 +0530

    Create SeleniumTest.java

---
+++ src/test/java/com/buddy/test/SeleniumTest.java
@@ -0,0 +1,47 @@
+package works.buddy.test;
+
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.Ignore;
+import org.openqa.selenium.*;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import static org.junit.Assert.*;
+import java.net.URL;
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+
+public class SeleniumTest {
+
+    private WebDriver driver;
+
+    @Before
+    public void setUp() throws MalformedURLException {
+        DesiredCapabilities capability = DesiredCapabilities.chrome();
+        driver = new RemoteWebDriver(new URL("http://34.93.120.219:4444/wd/hub"), capability);
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void test1() throws Exception {
+    	driver.get("http://34.93.73.51:8080/helloworld/");
+        assertEquals("Hello Buddy!", driver.getTitle());
+    }
+
+	//Error
+	@Ignore
+    @Test
+    public void test2() throws Exception {
+    	driver.get("http://34.93.73.51:8080/helloworld/");
+        assertEquals("Hello Rafal!", driver.getTitle());
+    }
+
+    @After
+    public void tearDown() {
+    	driver.quit();
+    }
+
+}
