5ae92c1cc530666eea9b8fb1be37537f62f8e6b9
rakesh635 <rakesh635@gmail.com>
2019-11-25 17:37:23 +0530

    Update SeleniumTest.java

--- src/test/java/com/buddy/test/SeleniumTest.java
+++ src/test/java/com/buddy/test/SeleniumTest.java
@@ -20,14 +20,16 @@ public class SeleniumTest {
     @Before
     public void setUp() throws MalformedURLException {
         DesiredCapabilities capability = DesiredCapabilities.chrome();
-        driver = new RemoteWebDriver(new URL("http://34.93.120.219:4444/wd/hub"), capability);
+        //driver = new RemoteWebDriver(new URL("http://34.93.120.219:4444/wd/hub"), capability);
+	driver = new RemoteWebDriver(new URL("http://34.93.123.206:4444/wd/hub"), capability);
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
     }
 
     @Test
     public void test1() throws Exception {
-    	driver.get("http://34.93.73.51:8080/helloworld/");
+    	//driver.get("http://34.93.73.51:8080/helloworld/");
+	driver.get("http://35.200.166.99:8081/hello2/");
         assertEquals("Demo Application - Landing Page", driver.getTitle());
     }
 
@@ -35,7 +37,8 @@ public class SeleniumTest {
 	@Ignore
     @Test
     public void test2() throws Exception {
-    	driver.get("http://34.93.73.51:8080/helloworld/");
+    	//driver.get("http://34.93.73.51:8080/helloworld/");
+	driver.get("http://35.200.166.99:8081/hello2/");
         assertEquals("Demo Application - Landing Page", driver.getTitle());
     }
 
