bb72c1fd89a7bc3c09e593d831ecbbc2c6a39d02
rakesh635 <rakesh635@gmail.com>
2019-11-26 15:55:27 +0530

    Update SeleniumTest.java

--- src/test/java/com/buddy/test/SeleniumTest.java
+++ src/test/java/com/buddy/test/SeleniumTest.java
@@ -27,11 +27,20 @@ chromeOptions.addArguments("--headless");
 desiredCapabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
 WebDriver driver = new RemoteWebDriver(url, desiredCapabilities);    */
 	    
+	    
+	ChromeOptions chromeOptions = new ChromeOptions();
+	chromeOptions.setBinary(getChromeExecutableLocation().getAbsolutePath());
+	chromeOptions.addArguments("no-sandbox");
+	DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+	  
+	    
 	System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");    
 	DesiredCapabilities capability = DesiredCapabilities.chrome();
         capability.setCapability("headless", true);
+	capability.setCapability("headless", true);
         capability.setCapability("platform", "LINUX");
         capability.setCapability("version", "ANY");
+	capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);  
         //driver = new RemoteWebDriver(new URL("http://34.93.120.219:4444/wd/hub"), capability);
 	driver = new RemoteWebDriver(new URL("http://34.93.123.206:4444/wd/hub"), capability);
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
