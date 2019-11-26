e95475ae6b238ef709267371d58b9e0b6e735bcb
rakesh635 <rakesh635@gmail.com>
2019-11-26 17:16:29 +0530

    Update SeleniumTest.java

--- src/test/java/com/buddy/test/SeleniumTest.java
+++ src/test/java/com/buddy/test/SeleniumTest.java
@@ -32,8 +32,8 @@ WebDriver driver = new RemoteWebDriver(url, desiredCapabilities);    */
 	//System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");    
 	DesiredCapabilities capability = DesiredCapabilities.chrome();
         //capability.setCapability("headless", true);
-        capability.setCapability("platform", "LINUX");
-        capability.setCapability("version", "ANY");
+        //capability.setCapability("platform", "LINUX");
+        //capability.setCapability("version", "ANY");
 	//capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptions);  
         //driver = new RemoteWebDriver(new URL("http://34.93.120.219:4444/wd/hub"), capability);
 	driver = new RemoteWebDriver(new URL("http://34.93.123.206:4444/wd/hub"), capability);
